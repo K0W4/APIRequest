bd4741df14f5dcbd2afcb6d9b05d925b86242b1c
Gabriel Kowaleski <kowa@MacBook-Pro-de-Kowa.local>
2025-08-13 15:51:44 -0300

    first commit

---
+++ APIRequest/APIRequest/APIRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIRequestApp.swift
+//  APIRequest
+//
+//  Created by Gabriel Kowaleski on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct APIRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
