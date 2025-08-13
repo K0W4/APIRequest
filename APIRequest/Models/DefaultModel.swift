f1cee7911f8d369e675c63a3cfc1ac4deff163a0
Gabriel Kowaleski <kowa@MacBook-Pro-de-Kowa.local>
2025-08-13 16:31:58 -0300

    Criando arquivos base

---
+++ APIRequest/Models/DefaultModel.swift
@@ -0,0 +1,8 @@
+//
+//  DefaultModel.swift
+//  APIRequest
+//
+//  Created by Gabriel Kowaleski on 13/08/25.
+//
+
+import Foundation
---
+++ APIRequest/Services/DefaultService.swift
@@ -0,0 +1,8 @@
+//
+//  DefaultService.swift
+//  APIRequest
+//
+//  Created by Gabriel Kowaleski on 13/08/25.
+//
+
+import Foundation
