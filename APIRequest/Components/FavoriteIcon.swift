44a9b3958710cdb6a681abe91af3b2f2081c3854
gabrielcbarbosa01 <gabrielcbarbosa001@gmail.com>
2025-08-14 15:35:29 -0300

    Criando componente de ProductCard e FavoriteIcon

---
+++ APIRequest/Components/FavoriteIcon.swift
@@ -0,0 +1,31 @@
+//
+//  FavoriteIcon.swift
+//  APIRequest
+//
+//  Created by Gabriel Barbosa on 13/08/25.
+//
+
+import SwiftUI
+
+struct FavoriteIcon: View {
+    
+    @Binding var isFavorite: Bool
+    
+    var body: some View {
+        
+        Image(systemName: isFavorite ? "heart.fill" : "heart")
+            .typography(.title3Regular)
+            .padding(8)
+            .background(
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundStyle(.fillsTertiary)
+            )
+            .onTapGesture {
+                isFavorite.toggle()
+            }
+    }
+}
+
+#Preview {
+    FavoriteIcon(isFavorite: .constant(false))
+}
---
+++ APIRequest/Components/ProductCard.swift
@@ -0,0 +1,96 @@
+//
+//  ProductCard.swift
+//  APIRequest
+//
+//  Created by Gabriel Barbosa on 13/08/25.
+//
+
+import SwiftUI
+
+struct ProductCard: View {
+    
+    var category: String = "Category"
+    var productName: String = "Product name with two or more lines goes here"
+    var productPrice: Double = 0.0
+    var isHorizontal: Bool = false
+    
+    @State var isFavorite: Bool = false
+    
+    var body: some View {
+        
+        if isHorizontal {
+            HStack(alignment: .top, spacing: 16) {
+                
+                Image("Placeholder")
+                    .resizable()
+                    .frame(width: 160, height: 160)
+                    .cornerRadius(8)
+                
+                VStack(alignment: .leading, spacing: 24) {
+                    
+                    HStack(spacing: 4) {
+                        Text("\(category)".uppercased())
+                            .typography(.footnoteRegular)
+                            .foregroundStyle(Color(.labelsSecondary))
+                        
+                        Spacer()
+   
+                        FavoriteIcon(isFavorite: $isFavorite)
+                    }
+                    
+                    VStack (alignment: .leading, spacing: 4) {
+                        Text("\(productName)")
+                            .typography(.subheadlineRegular)
+                            .lineLimit(2)
+                                                
+                        let formattedPrice = String(format: "%.2f", productPrice)
+                        Text("US$ \(formattedPrice)")
+                            .typography(.headline)
+                    }
+                }
+            }
+            .padding(8)
+            .background(
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundStyle(Color(.backgroundsSecondary))
+            )
+        }
+        
+        else {
+            
+            VStack(alignment: .leading, spacing: 8) {
+                
+                ZStack(alignment: .topTrailing) {
+                    
+                    Image("Placeholder")
+                        .resizable()
+                        .frame(width: 160, height: 160)
+                        .cornerRadius(8)
+                    
+                    FavoriteIcon(isFavorite: $isFavorite)
+                    
+                }
+                
+                VStack(alignment: .leading ,spacing: 4) {
+                    Text("\(productName)")
+                        .typography(.subheadlineRegular)
+                        .lineLimit(2)
+                    
+                    let formattedPrice = String(format: "%.2f", productPrice)
+                    Text("US$ \(formattedPrice)")
+                        .typography(.headline)
+                }
+            }
+            .padding(8)
+            .background(
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundStyle(Color(.backgroundsSecondary))
+            )
+            .frame(maxWidth: 177)
+        }
+    }
+}
+
+#Preview {
+    ProductCard()
+}
