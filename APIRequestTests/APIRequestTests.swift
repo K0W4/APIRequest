79826d92da92038b420fba77b563f77d259e034d
Gabriel Kowaleski <kowa@MacBook-Pro-de-Kowa.local>
2025-08-18 16:24:45 -0300

    Iniciando teste e API

---
+++ APIRequestTests/APIRequestTests.swift
@@ -0,0 +1,16 @@
+//
+//  APIRequestTests.swift
+//  APIRequestTests
+//
+//  Created by Gabriel Kowaleski on 18/08/25.
+//
+
+import Testing
+
+struct APIRequestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
